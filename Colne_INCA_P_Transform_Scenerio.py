f1a3c75df4285999e8faddd97353965b787937be
Zakriya Ahmad <143161483+ZakriyaAhmad@users.noreply.github.com>
2025-11-02 01:59:39 +0500

    Update Colne_INCA_P_Transform_Scenerio.py

--- Colne_INCA_P_Transform_Scenerio.py
+++ Colne_INCA_P_Transform_Scenerio.py
@@ -5,11 +5,11 @@ import boto3
 from io import StringIO
 
 # Replace with your AWS credentials or use IAM roles and instance profiles
-aws_access_key_id = 'AKIA3YZJKPFW6AFKH4M4'
-aws_secret_access_key = 'Yqf50oTXj6F8qNfKrjqoxyuGZZGayVab4x/XDPeR'
+aws_access_key_id = ''
+aws_secret_access_key = '/XDPeR'
 region_name = 'eu-west-1'  
-source_bucket_name = 'scenerio-automation'
-source_file_key = 'inca-model-output-files/Colne_INCA-P.dsd'  # Replace with the path of the source file in the source S3 bucket
+source_bucket_name = ''
+source_file_key = ''  # Replace with the path of the source file in the source S3 bucket
 destination_bucket_name = 'scenerio-automation'
 destination_file_key = 'Transform-CSV-Files/Colne_INCA-P.csv'# Replace with the path of the destination file in the destination S3 bucket
 
