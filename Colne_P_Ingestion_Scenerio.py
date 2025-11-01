323a5d70c908b7f3ef5881df237d5001bad71b4a
Zakriya Ahmad <143161483+ZakriyaAhmad@users.noreply.github.com>
2025-11-02 02:02:09 +0500

    Update Colne_P_Ingestion_Scenerio.py

--- Colne_P_Ingestion_Scenerio.py
+++ Colne_P_Ingestion_Scenerio.py
@@ -8,10 +8,10 @@ from psycopg2.extras import execute_values
 
 
 # AWS S3 credentials and bucket information
-s3_access_key_id = 'AKIA3YZJKPFW6AFKH4M4'
-s3_secret_access_key = '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'
+s3_access_key_id = ''
+s3_secret_access_key = '/XDPeR'
 region_name = 'eu-west-1' 
-bucket_name = 'scenerio-automation'
+bucket_name = ''
 folder_name = 'Transform-CSV-Files'
 file_name = 'Colne_INCA-P.csv'
 SCHEMA_NAME = 'aquascope_mvp_bkp'
@@ -36,11 +36,11 @@ def process_colne_p_s3_to_pgadmin():
 
     # Open a cursor to perform database operations
     cursor = conn.cursor()
-    truncate_query = "TRUNCATE TABLE aquascope_mvp_bkp.colne_inca_p_scenerio"
+    truncate_query = "TRUNCATE TABLE colne_inca_p_scenerio"
     cursor.execute(truncate_query)
     # Construct the INSERT query
     # Prepare the query for data insertion
-    insert_query = f"INSERT INTO aquascope_mvp_bkp.colne_inca_p_scenerio (discharge, volume, velocity, water_depth, stream_power, shear_velocity, max_ent_grain_size, moveable_bed_mass, entrainment_rate, deposition_rate, bed_sediment, suspended_sediment, diffuse_sediment, water_column_tdp, water_column_pp, wc_sorption_release, stream_bed_tdp, stream_bed_pp, bed_sorption_release, macrophyte_mass, epiphyte_mass, water_column_tp, water_column_srp, water_temperature, tdp_input, pp_input, water_column_epc0, stream_bed_epc0, suspended_sediment_mass, mprop, settling_velocity, r, rmax, live_phytoplankton, dissolved_oxygen, bod, _saturation, reach, date_) VALUES %s"
+    insert_query = f"INSERT INTO colne_inca_p_scenerio (discharge, volume, velocity, water_depth, stream_power, shear_velocity, max_ent_grain_size, moveable_bed_mass, entrainment_rate, deposition_rate, bed_sediment, suspended_sediment, diffuse_sediment, water_column_tdp, water_column_pp, wc_sorption_release, stream_bed_tdp, stream_bed_pp, bed_sorption_release, macrophyte_mass, epiphyte_mass, water_column_tp, water_column_srp, water_temperature, tdp_input, pp_input, water_column_epc0, stream_bed_epc0, suspended_sediment_mass, mprop, settling_velocity, r, rmax, live_phytoplankton, dissolved_oxygen, bod, _saturation, reach, date_) VALUES %s"
     print(insert_query)
 
     # Execute the INSERT query with the data
